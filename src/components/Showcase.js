5729b5c5a769bdb6536233be8e08ad1339af1d06
Jetal C. Mali <malijetal1234@gmail.com>
2021-09-04 00:11:32 +0530

    Showcase part-2

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -4,18 +4,21 @@ import hero from "../images/hero.jpg";
 const Showcase = () => {
   return (
     <div className="Showcase">
-      <section className="bg-green-300 pt-32 h-screen">
+      <section className="bg-green-300 py-16 px-8 md:grid md:grid-cols-2 md:gap-20 h-3/4">
         <div>
-          <h1>Photo Editing and Graphic Design Made for Everyone.</h1>
-          <p>
+          <h1 className="font-bold-1 text-5xl mb-6">
+            Photo Editing and Graphic Design Made for Everyone.
+          </h1>
+          <p className="mb-6">
             BeFunky's all-in-one online Creative Platform has everything you
             need to easily edit photos, create graphic designs, and make photo
             collages.
           </p>
+          <button className="button button-xl bg-blue-700 text-white p-4 px-8 rounded-md cursor-pointer transition-all hover:opacity-75">
+            Get Started
+          </button>
         </div>
-        <div>
-          <img src="hero" alt="" />
-        </div>
+        <div className="slider">{/* <img src={hero} alt="" /> */}</div>
       </section>
     </div>
   );
